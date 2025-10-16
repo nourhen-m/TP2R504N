273fc1d51ee155ffc1ea8fbc4dd937a93ac9d9d1
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 09:09:04 +0200

    test: vérifier que affiche() retourne une chaîne (FAIL)

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,17 +1,10 @@
 import unittest
-import sys
-from io import StringIO
 from fizzbuzz import affiche
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_affiche_commence_par_12_et_contient_Fizz(self):
-        sortie = StringIO()
-        sys.stdout = sortie
-        affiche()
-        resultat = sortie.getvalue().strip()
-
-        self.assertTrue(resultat.startswith("12"), "Doit commencer par '12'")
-        self.assertIn("Fizz", resultat, "3 doit être remplacé par Fizz")
+    def test_affiche_retourne_du_texte(self):
+        resultat = affiche()
+        self.assertIsInstance(resultat, str, "affiche() doit retourner une chaîne de caractères")
 
 if __name__ == "__main__":
     unittest.main()
