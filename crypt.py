fd0ade6e3505c9af83b33d9c2f5a85398beb7f1a
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 10:32:12 +0200

    feat(Q2-A4): implémentation du décalage générique (SUCCESS)

--- crypt.py
+++ crypt.py
@@ -1,4 +1,15 @@
+import string
+
+CARACTERES = string.ascii_letters + string.punctuation + string.digits + " "
+
 def crypt(message: str) -> str:
     if message == "":
         return ""
-    return ""
+    resultat = ""
+    for ch in message:
+        if ch in CARACTERES:
+            index = CARACTERES.index(ch)
+            resultat += CARACTERES[(index + 1) % len(CARACTERES)]
+        else:
+            resultat += ch
+    return resultat
