bcdd4a0704543b750dac32fd0aa429fa8237fb49
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 09:45:48 +0200

    feat(C): ajout des deux paramètres n1 et n2 à affiche() (SUCCESS)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,4 @@
-def affiche(n):
-    texte = ""
-    for i in range(1, n + 1):
+def affiche(n1, n2):
+    return ""
 
-        if i % 15 == 0:
-            texte += "FrisBee"
-        elif i % 3 == 0:
-            texte += "Fizz"
-        elif i % 5 == 0:
-            texte += "Buzz"
-        else:
-            texte += str(i)
-
-    return texte
 
