b0146dbd9a5d4a7da32a525d08ff6f124552e667
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 09:12:13 +0200

    feat: affiche() utilise une boucle pour générer 1→5 (SUCCESS)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,12 @@
 def affiche():
-    return "12Fizz4Buzz"
+    texte = ""
+    for i in range(1, 6):
+        if i % 15 == 0:
+            texte += "FrisBee"
+        elif i % 3 == 0:
+            texte += "Fizz"
+        elif i % 5 == 0:
+            texte += "Buzz"
+        else:
+            texte += str(i)
+    return texte
