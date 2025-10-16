67b194b9fc0282fc146bef48ab2bdc9b31ebc1e0
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 09:03:55 +0200

    test: vérifier que 3 est remplacé par Fizz (FAIL)

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,15 +4,14 @@ from io import StringIO
 from fizzbuzz import affiche
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_affiche_commence_par_12(self):
+    def test_affiche_commence_par_12_et_contient_Fizz(self):
         sortie = StringIO()
         sys.stdout = sortie
-
         affiche()
-
-
         resultat = sortie.getvalue().strip()
-        self.assertTrue(resultat.startswith("12"), "La sortie doit commencer par '12'")
+
+        self.assertTrue(resultat.startswith("12"), "Doit commencer par '12'")
+        self.assertIn("Fizz", resultat, "3 doit être remplacé par Fizz")
 
 if __name__ == "__main__":
     unittest.main()
