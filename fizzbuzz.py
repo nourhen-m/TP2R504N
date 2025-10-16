c3e01374d79c2bc9267e0d3abd1fba6092d95b47
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 09:53:14 +0200

    feat(C): affiche(n1, n2) affiche la séquence correcte de n1 à n2 (SUCCESS)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,16 @@
 def affiche(n1, n2):
-    return ""
+    texte = ""
+    for i in range(n1, n2 + 1):
+        if i % 15 == 0:
+            texte += "FrisBee"
+        elif i % 3 == 0:
+            texte += "Fizz"
+        elif i % 5 == 0:
+            texte += "Buzz"
+        else:
+            texte += str(i)
+
+
+    return texte
 
 
