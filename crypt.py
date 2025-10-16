209688444116f50a4c9c772cc1190459c83ce8ae
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 11:16:37 +0200

    feat(Q2-B4): gestion du décalage par pas (SUCCESS)

--- crypt.py
+++ crypt.py
@@ -6,8 +6,9 @@ def crypt(message: str, pas: int = 1) -> str:
     resultat = ""
     for ch in message:
         if ch in CARACTERES:
-            index = CARACTERES.index(ch)
-            resultat += CARACTERES[(index + 1) % len(CARACTERES)]
+            i = CARACTERES.index(ch)
+            resultat += CARACTERES[(i + pas) % len(CARACTERES)]
+
         else:
             resultat += ch
     return resultat
