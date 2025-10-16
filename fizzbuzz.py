08d19f2ed4f4556db08ce3575ef6c23633a453f3
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 09:23:30 +0200

    feat(B): signature affiche(n) qui retourne une chaîne (SUCCESS)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,2 @@
-def affiche():
-    texte = ""
-    for i in range(1, 101):
-        if i % 15 == 0:
-            texte += "FrisBee"
-        elif i % 3 == 0:
-            texte += "Fizz"
-        elif i % 5 == 0:
-            texte += "Buzz"
-        else:
-            texte += str(i)
-    return texte
+def affiche(n):
+    return ""
