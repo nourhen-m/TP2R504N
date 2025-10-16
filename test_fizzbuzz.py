1c96a102d64044ae623178bfc261c299a0357ffa
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 09:44:37 +0200

    test(C): vérifier que affiche(n1, n2) peut être appelée (FAIL)

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,24 +3,8 @@ from fizzbuzz import affiche
 
 class TestFizzBuzzPartieB(unittest.TestCase):
     def test_affiche_accepte_un_parametre(self):
-        resultat = affiche(1)
-        self.assertIsInstance(resultat, str)
-
-    def test_affiche_1(self):
-        self.assertEqual(affiche(1), "1")
-
-    def test_affiche_2(self):
-        self.assertEqual(affiche(2), "12")
-    def test_fizz_pour_3(self):
-        self.assertEqual(affiche(3), "12Fizz")
-
-    def test_buzz_pour_5(self):
-        self.assertEqual(affiche(5), "12Fizz4Buzz")
-
-
-    def test_sequence_complete_jusqua_15(self):
-        attendu = "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee"
-        self.assertEqual(affiche(15), attendu)
+        resultat = affiche(5, 10)
+        self.assertIsInstance(resultat, str, "affiche(n1, n2) doit retourner une chaîne")
 
 
 
