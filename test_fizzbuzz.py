2d2c92701f38669d4046ffe64424c6b28c375730
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 09:26:38 +0200

    test(B): pour n=2, résultat attendu '12' (FAIL)

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,11 +4,13 @@ from fizzbuzz import affiche
 class TestFizzBuzzPartieB(unittest.TestCase):
     def test_affiche_accepte_un_parametre(self):
         resultat = affiche(1)
-        self.assertIsInstance(resultat, str, "affiche(n) doit retourner une chaîne")
+        self.assertIsInstance(resultat, str)
 
     def test_affiche_1(self):
         self.assertEqual(affiche(1), "1")
 
+    def test_affiche_2(self):
+        self.assertEqual(affiche(2), "12")
 
 
 if __name__ == "__main__":
