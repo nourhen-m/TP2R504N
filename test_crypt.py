9ea7dc826ae2969f7da9a0b5404a62e8547953ac
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 11:37:42 +0200

    feat(Q2-C2): squelette minimal de decrypt() (SUCCESS)

--- test_crypt.py
+++ test_crypt.py
@@ -1,22 +1,11 @@
 import unittest
 from crypt import crypt
-
 class TestCryptage(unittest.TestCase):
     def test_existence_et_type(self):
         self.assertIsInstance(crypt(""), str)
 
     def test_a_devient_b(self):
         self.assertEqual(crypt("a"), "b")
-
-    def test_crypt_accepte_un_pas(self):
-        try:
-            resultat = crypt("a", 2)
-        except TypeError:
-            self.fail("crypt() doit accepter 2 arguments (message, pas)")
-
-
-    def test_a_devient_c_avec_pas_2(self):
-        self.assertEqual(crypt("a", 2), "c2")
 
 
 
