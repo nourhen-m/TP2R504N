cb84c70bbc5edf357b5ea27f4f79be075b9d3e10
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 11:40:37 +0200

    feat(Q2-C4): decrypt() inverse crypt() pour pas=1 (SUCCESS)

--- decrypt.py
+++ decrypt.py
@@ -1,4 +1,17 @@
+import string
+
+CARACTERES = string.ascii_letters + string.punctuation + string.digits + " "
+
 def decrypt(message: str) -> str:
     if message == "":
         return ""
-    return ""
+
+    resultat = ""
+    for ch in message:
+        if ch in CARACTERES:
+            i = CARACTERES.index(ch)
+            resultat += CARACTERES[(i - 1) % len(CARACTERES)]
+        else:
+            resultat += ch
+    return resultat
+
