693d68da04ab673f7b675e85880d60db576e0ab4
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 09:30:41 +0200

    feat(B): implémentation de la règle Fizz (SUCCESS)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,11 @@
 def affiche(n):
     texte = ""
     for i in range(1, n + 1):
-        texte += str(i)
+
+        if i % 3 == 0:
+            texte += "Fizz"
+        else:
+            texte += str(i)
+
     return texte
 
