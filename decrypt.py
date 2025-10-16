ca789fe4b2e64fe7d80c38ac3a43cfcd3da83cae
nourhen-m <messaadinourhen@gmail.com>
2025-10-16 11:46:14 +0200

    feat(Q2-C6): decrypt() gère le pas et inverse correctement crypt() (SUCCESS)

--- decrypt.py
+++ decrypt.py
@@ -6,12 +6,16 @@ def decrypt(message: str) -> str:
     if message == "":
         return ""
 
+    pas = 1
+    if message[-1].isdigit():
+        pas = int(message[-1])
+        message = message[:-1]
+
     resultat = ""
     for ch in message:
         if ch in CARACTERES:
             i = CARACTERES.index(ch)
-            resultat += CARACTERES[(i - 1) % len(CARACTERES)]
+            resultat += CARACTERES[(i - pas) % len(CARACTERES)]
         else:
             resultat += ch
     return resultat
-
